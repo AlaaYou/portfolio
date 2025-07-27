95c6e258864369f15c2b1d6722d6bc2961f90602
AlaaYou <47208437+AlaaYou@users.noreply.github.com>
2025-07-27 14:12:39 +0100

    Update Tech.jsx
    color pytorch

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -41,7 +41,7 @@ const Tech = () => {
         initial="initial"
         animate="animate"
         className="rounded-2xl border-4 border-neutral-800 p-4">
-            <SiPytorch className="text-7xl text-orange-600"/>
+            <SiPytorch className="text-7xl text-orange-800"/>
         </motion.div>
         
         <motion.div 
